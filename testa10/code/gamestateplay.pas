485efc150ab757ab00e1482f5aaed50256323568
and3md <and3md@gmail.com>
2021-07-27 13:35:48 +0200

    Added two test project.

---
+++ testa10/code/gamestateplay.pas
@@ -0,0 +1,163 @@
+{ Main "playing game" state, where most of the game logic takes place.
+
+  Feel free to use this code as a starting point for your own projects.
+  (This code is in public domain, unlike most other CGE code which
+  is covered by the LGPL license variant, see the COPYING.txt file.) }
+unit GameStatePlay;
+
+interface
+
+uses Classes,
+  CastleUIState, CastleComponentSerialize, CastleUIControls, CastleControls,
+  CastleKeysMouse, CastleViewport, CastleScene, CastleVectors, CastleCameras,
+  CastleTransform,
+  GameEnemy;
+
+type
+  { Main "playing game" state, where most of the game logic takes place. }
+  TStatePlay = class(TUIState)
+  private
+    { Components designed using CGE editor, loaded from gamestateplay.castle-user-interface. }
+    LabelFps: TCastleLabel;
+    MainViewport: TCastleViewport;
+    WalkNavigation: TCastleWalkNavigation;
+    Button: TCastleButton;
+
+    { Enemies behaviors }
+    Enemies: TEnemyList;
+  public
+    constructor Create(AOwner: TComponent); override;
+    procedure Start; override;
+    procedure Stop; override;
+    procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
+    function Press(const Event: TInputPressRelease): Boolean; override;
+    procedure ClickButton(Sender: TObject);
+  end;
+
+var
+  StatePlay: TStatePlay;
+
+implementation
+
+uses SysUtils, Math,
+  CastleSoundEngine, CastleLog, CastleStringUtils, CastleFilesUtils,
+  GameStateMenu;
+
+{ TStatePlay ----------------------------------------------------------------- }
+
+constructor TStatePlay.Create(AOwner: TComponent);
+begin
+  inherited;
+  DesignUrl := 'castle-data:/gamestateplay.castle-user-interface';
+end;
+
+procedure TStatePlay.Start;
+var
+  SoldierScene: TCastleScene;
+  Enemy: TEnemy;
+  I: Integer;
+begin
+  inherited;
+
+  { Find components, by name, that we need to access from code }
+  LabelFps := DesignedComponent('LabelFps') as TCastleLabel;
+  MainViewport := DesignedComponent('MainViewport') as TCastleViewport;
+  {WalkNavigation := DesignedComponent('WalkNavigation') as TCastleWalkNavigation;}
+  Button := DesignedComponent('Button1') as TCastleButton;
+  Button.OnClick := @ClickButton;
+
+{  { Create TEnemy instances, add them to Enemies list }
+  Enemies := TEnemyList.Create(true);
+  for I := 1 to 4 do
+  begin
+    SoldierScene := DesignedComponent('SceneSoldier' + IntToStr(I)) as TCastleScene;
+    { Below using nil as Owner of TEnemy, as the Enemies list already "owns"
+      instances of this class, i.e. it will free them. }
+    Enemy := TEnemy.Create(nil);
+    SoldierScene.AddBehavior(Enemy);
+    Enemies.Add(Enemy);
+  end;}
+end;
+
+procedure TStatePlay.Stop;
+begin
+  FreeAndNil(Enemies);
+  inherited;
+end;
+
+procedure TStatePlay.Update(const SecondsPassed: Single; var HandleInput: Boolean);
+begin
+  inherited;
+  { This virtual method is executed every frame.}
+  LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+
+  //WritelnLog('Direction ' + MainViewport.Camera.Direction.ToString);
+  //WritelnLog('Position ' + MainViewport.Camera.Position.ToString);
+end;
+
+function TStatePlay.Press(const Event: TInputPressRelease): Boolean;
+var
+  HitEnemy: TEnemy;
+begin
+  Result := inherited;
+  if Result then Exit; // allow the ancestor to handle keys
+
+  { This virtual method is executed when user presses
+    a key, a mouse button, or touches a touch-screen.
+
+    Note that each UI control has also events like OnPress and OnClick.
+    These events can be used to handle the "press", if it should do something
+    specific when used in that UI control.
+    The TStatePlay.Press method should be used to handle keys
+    not handled in children controls.
+  }
+
+  if Event.IsMouseButton(buttonLeft) then
+  begin
+    SoundEngine.Sound(SoundEngine.SoundFromName('shoot_sound'));
+
+    { We clicked on enemy if
+      - TransformUnderMouse indicates we hit something
+      - It has a behavior of TEnemy. }
+    if (MainViewport.TransformUnderMouse <> nil) and
+       (MainViewport.TransformUnderMouse.FindBehavior(TEnemy) <> nil) then
+    begin
+      HitEnemy := MainViewport.TransformUnderMouse.FindBehavior(TEnemy) as TEnemy;
+      HitEnemy.Hurt;
+    end;
+
+    Exit(true);
+  end;
+
+  {if Event.IsKey(CtrlM) then
+  begin
+    WalkNavigation.MouseLook := not WalkNavigation.MouseLook;
+    Exit(true);
+  end;}
+
+  if Event.IsKey(keyF5) then
+  begin
+    Container.SaveScreenToDefaultFile;
+    Exit(true);
+  end;
+
+  if Event.IsKey(keyEscape) then
+  begin
+    TUIState.Current := StateMenu;
+    Exit(true);
+  end;
+end;
+
+procedure TStatePlay.ClickButton(Sender: TObject);
+begin
+  {MainViewport.Camera.Position := Vector3(0.97,0.00,0.26);
+  MainViewport.Camera.Direction := Vector3(-0.30, 1.82, -2.66);}
+
+  MainViewport.Camera.Position := Vector3(1.95, 1.48, -1.94);
+  MainViewport.Camera.Direction := Vector3(0.95, 0.00, 0.31);
+
+  MainViewport.Camera.Position := Vector3(1.92, 1.48, -1.95);
+  MainViewport.Camera.Direction := Vector3(0.95, 0.00, 0.30);
+end;
+
+end.
